70f8b8101e3d803457f6197d85371b37afa12c3b
saitejayeniganti <42708541+saitejayeniganti@users.noreply.github.com>
2021-12-08 12:16:23 -0800

    setup

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,14 +1,41 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes/routes";
+import { ApolloProvider } from "react-apollo";
+
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://localhost:8080/graphql",
+});
+
+const client = new ApolloClient({
+  link: link,
+  cache: cache,
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "no-cache",
+      partialRefetch: true,
+    },
+    query: {
+      fetchPolicy: "no-cache",
+      partialRefetch: true,
+    },
+  },
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Routes />
-      </div>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <div>
+          <Routes />
+        </div>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
