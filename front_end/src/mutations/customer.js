9781bdaee1a9ee867adf9fd74192c7d56f277d2d
saitejayeniganti <42708541+saitejayeniganti@users.noreply.github.com>
2021-12-08 13:36:53 -0800

    update customer.js

--- front_end/src/mutations/customer.js
+++ front_end/src/mutations/customer.js
@@ -11,11 +11,48 @@ const customersignupMutation = gql`
 `;
 
 const customerDetailsMutation = gql`
-  mutation ($email: String, $password: String, $mobile: String) {
-    customersignup(email: $email, password: $password, mobile: $mobile) {
-      message
-      id
-      status
+  mutation (
+    $name: String
+    $email_id: String
+    $mobile: String
+    $city: String
+    $state: String
+    $country: String
+    $nick_name: String
+    $about: String
+    $image_url: String
+    $address: String
+    $latitude: String
+    $longitude: String
+    $id: String
+  ) {
+    customerDetails( $name: String
+        email_id: $email_id
+        mobile: $mobile
+        city:  $city
+        state: $state
+        country: $country
+        nick_name: $nick_name
+        about: $about
+        image_url: $image_url
+        address: $address
+        latitude:  $latitude
+        longitude: $longitude
+        id: $id) {
+        email_id,
+        mobile,
+        city,
+        state,
+        country,
+        nick_name,
+        about,
+        image_url,
+        address,
+        latitude,
+        longitude,
+        id
     }
   }
 `;
+
+export { customersignupMutation, customerDetailsMutation };
