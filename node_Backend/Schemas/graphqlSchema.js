811db479625a3d17dee213437920512f00f87ee6
saitejayeniganti <42708541+saitejayeniganti@users.noreply.github.com>
2021-12-08 17:17:36 -0800

    Update graphqlSchema.js

--- node_Backend/Schemas/graphqlSchema.js
+++ node_Backend/Schemas/graphqlSchema.js
@@ -160,36 +160,54 @@ const Response = new GraphQLObjectType({
   }),
 });
 
-const Mutation = new GraphQLObjectType({
-  name: "Mutation",
-  fields: {
-    CustomerDetails: {
-      type: CustomersignupDetails,
-      args: {
-        id: { type: GraphQLInt },
-        name: { type: GraphQLString },
-        mobile: { type: GraphQLString },
-        email_id: { type: GraphQLString },
-        password: { type: GraphQLString },
-        city: { type: GraphQLString },
-        state: { type: GraphQLString },
-        country: { type: GraphQLString },
-        address: { type: GraphQLString },
-        nick_name: { type: GraphQLString },
-        about: { type: GraphQLString },
-        latitude: { type: GraphQLString },
-        longitude: { type: GraphQLString },
-        image_url: { type: GraphQLString },
-      },
-      async resolve(parent, args) {
-        console.log("In resolve");
-        return await customer.updateCustomer(args);
-      },
-    },
-  },
-});
-const schema = new GraphQLSchema({
-  mutation: Mutation,
+const RestaurantResponse = new GraphQLObjectType({
+  name: "restaurantResponse",
+  fields: () => ({
+    status: { type: GraphQLString },
+    body: { type: Restaurant },
+  }),
 });
 
-module.exports = schema;
+const UserInputType = new GraphQLInputObjectType({
+  name: "userInputType",
+  fields: () => ({
+    _id: { type: GraphQLString },
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    nickname: { type: GraphQLString },
+    password: { type: GraphQLString },
+    number: { type: GraphQLString },
+    email: { type: GraphQLString },
+    dob: { type: GraphQLString },
+    address: { type: GraphQLString },
+    imageUrl: { type: GraphQLString },
+    favorites: { type: GraphQLString },
+    additionalAddresses: { type: GraphQLString },
+  }),
+});
+
+const OrderInputType = new GraphQLInputObjectType({
+  name: "orderInputType",
+  fields: () => ({
+    _id: { type: GraphQLString },
+    id: { type: GraphQLString },
+    description: { type: GraphQLString },
+    totalCost: { type: GraphQLFloat },
+    dateTime: { type: GraphQLString },
+    deliveryStatus: { type: GraphQLString },
+    status: { type: GraphQLString },
+    deliveryType: { type: GraphQLString },
+    customerID: { type: GraphQLString },
+    restaurantID: { type: GraphQLString },
+    address: { type: GraphQLString },
+    additionalAddresses: { type: GraphQLString },
+    deliveryNote: { type: GraphQLString },
+    name: { type: GraphQLString },
+    nickname: { type: GraphQLString },
+    number: { type: GraphQLString },
+    email: { type: GraphQLString },
+    dob: { type: GraphQLString },
+    imageUrl: { type: GraphQLString },
+    favorites: { type: GraphQLString },
+  }),
+});
